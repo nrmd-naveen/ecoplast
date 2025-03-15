c6917b4933626bfc360688f3b1add37cfb44ce51
nrmd-naveen <nrmdnaveen@gmail.com>
2025-03-15 15:33:12 +0530

    Hero Section Aligned

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return <>
         <div className="w-full z-50 fixed top-9 flex items-center justify-center">
 
-        <div className="w-[80%] h-15 md:h-[68px] md:w-[50%] bg-gradient-to-r from-white/80 md:via-[#81B121]/10 to-white/15 backdrop-blur-3xl px-4 py-3 flex items-center justify-between rounded-lg border-[.5px] border-neutral-600/15">
+        <div className="w-[80%] h-15 md:h-[68px] md:w-[50%] bg-white/70 backdrop-blur-3xl px-4 py-3 flex items-center justify-between rounded-lg border-[.5px] border-neutral-600/15">
             <div className="flex max-w-[50%] items-center gap-4">
                     <Image
                         src={logo}
@@ -17,13 +17,13 @@ const NavBar = () => {
                 <h1 className="md:text-xl font-bold"></h1>
             </div>
             <div className="min-w-[50%]  flex justify-end md:items-center md:gap-20 pr-5 md:pr-10 md:text-lg font-bold"> 
-                <a href="#" className="hidden md:block text-[#81b121] tracking-wide hover:text-neutral-100">
+                <a href="#" className="hidden md:block text-[#81b121] tracking-wide hover:text-neutral-600">
                     Services
                 </a>
-                <a href="#" className="hidden md:block text-[#81b121] tracking-wide hover:text-neutral-100">
+                <a href="#" className="hidden md:block text-[#81b121] tracking-wide hover:text-neutral-600">
                     Gallery
                 </a>
-                <a href="#" className="text-[#81b121] tracking-wide hover:text-neutral-100">
+                <a href="#" className="text-[#81b121] tracking-wide hover:text-neutral-600">
                     Contact Us
                 </a>
             </div>
