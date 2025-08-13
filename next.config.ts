d021de98900e39e85f7e35b3eacecc154a636450
nrmd-naveen <nrmdnaveen@gmail.com>
2025-08-13 19:39:32 +0530

    updated config added image domains

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
 
 module.exports = {
   images: {
-    domains: ['images.unsplash.com', 'ecoplastinteriors.s3.eu-north-1.amazonaws.com'],
+    domains: ['images.unsplash.com', 'ecoplastinteriors.s3.eu-north-1.amazonaws.com', 'realplast.in', 'www.zmartbuild.com', 'homerenot.com', 'blueskywindows.com.au', 'plasticranger.com'],
   },
 }
 
